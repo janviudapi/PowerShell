5da664fd267a8c06be4aa1d5b2fce94d6fc67e12
Janvi udapi <60569075+janviudapi@users.noreply.github.com>
2021-08-31 15:50:24 +0530

    Update ServerAssetInventory.ps1

--- HTML/HTML_Reporting01/ServerAssetInventory.ps1
+++ HTML/HTML_Reporting01/ServerAssetInventory.ps1
@@ -1,6 +1,5 @@
-﻿
 Import-Module -Name ActiveDirectory
-$servers = Get-AdComputer -Filter *
+$servers = Get-AdComputer -Filter {DNSHostName -like '*'} -Server 192.168.34.11 -Credential $credential
 
 #$servers = 'localhost', $env:COMPUTERNAME,  '127.0.0.1', 'nullserver', '192.168.34.107'
 $result = @()
@@ -12,34 +11,34 @@ foreach ($computerName in $servers) {
     {
         try
         {
-            $cimsession = New-CimSession -ComputerName $hostName -SessionOption $cimSessionOption -Credential $credential -ErrorAction Stop
-            Write-Host "Working on Server $hostName" -BackgroundColor DarkGreen
+            $cimsession = New-CimSession -ComputerName $computerName.DNSHostName -SessionOption $cimSessionOption -Credential $credential -ErrorAction Stop
+            Write-Host "Working on Server $($computerName.DNSHostName)" -BackgroundColor DarkGreen
             #Machine 
-            $computerSystem = Get-CimInstance -Class CIM_ComputerSystem -ComputerName $hostName -Cimsession $cimsession | Select-Object Manufacturer, Model -ErrorAction Stop #Win32_ComputerSystem 
+            $computerSystem = Get-CimInstance -Class CIM_ComputerSystem -CimSession $cimsession -ErrorAction Stop | Select-Object Manufacturer, Model -ErrorAction Stop #Win32_ComputerSystem 
             #serial Number
-            $bios = Get-CimInstance -Class Win32_BIOS  -ComputerName $hostName | Select-Object SerialNumber, SMBIOSBIOSVersion -ErrorAction Stop
+            $bios = Get-CimInstance -Class Win32_BIOS -CimSession $cimsession -ErrorAction Stop  | Select-Object SerialNumber, SMBIOSBIOSVersion -ErrorAction Stop
             #Motherboad
-            $baseBoard = Get-CimInstance -Class win32_baseboard -ComputerName $hostName | Select-Object Manufacturer, Product, SerialNumber, Version -ErrorAction Stop
+            $baseBoard = Get-CimInstance -Class win32_baseboard  -CimSession $cimsession -ErrorAction Stop | Select-Object Manufacturer, Product, SerialNumber, Version -ErrorAction Stop
             #Operating System:
-            $operatingSystem = Get-CimInstance -Class CIM_OperatingSystem -ComputerName $hostName | select-Object Caption, OSArchitecture -ErrorAction Stop #win32_OperatingSystem
+            $operatingSystem = Get-CimInstance -Class CIM_OperatingSystem -CimSession $cimsession -ErrorAction Stop | select-Object Caption, OSArchitecture -ErrorAction Stop #win32_OperatingSystem
             #Processor:
-            $processor = Get-CimInstance -Class CIM_Processor -ComputerName $hostName | select-Object Name, OSArchitecture, NumberOfCores, NumberOfEnabledCore, NumberOfLogicalProcessors, ProcessorId, PartNumber -ErrorAction Stop #Win32_Processor
+            $processor = Get-CimInstance -Class CIM_Processor -CimSession $cimsession -ErrorAction Stop | select-Object Name, OSArchitecture, NumberOfCores, NumberOfEnabledCore, NumberOfLogicalProcessors, ProcessorId, PartNumber -ErrorAction Stop #Win32_Processor
             #Memory
-            $physicalMemory = Get-CimInstance -Class CIM_PhysicalMemory -ComputerName $hostName | Select-Object DeviceLocator, SerialNumber, Capacity, @{N= "Speed";Expression = {if (($_.speed -ge '1000000000')) {"$($_.Speed / 1000000000) Gb/s"} Elseif ($_.Speed -gt 0) {"$($_.Speed / 1000000) Mb/s"}}}, 
+            $physicalMemory = Get-CimInstance -Class CIM_PhysicalMemory -CimSession $cimsession -ErrorAction Stop | Select-Object DeviceLocator, SerialNumber, Capacity, @{N= "Speed";Expression = {if (($_.speed -ge '1000000000')) {"$($_.Speed / 1000000000) Gb/s"} Elseif ($_.Speed -gt 0) {"$($_.Speed / 1000000) Mb/s"}}}, 
                     PartNumber, Manufacturer -ErrorAction Stop
             #Video Card
-            $videoController = Get-CimInstance -Class win32_VideoController -ComputerName $hostName | Select-Object Name, VideoProcessor -ErrorAction Stop
+            $videoController = Get-CimInstance -Class win32_VideoController -CimSession $cimsession -ErrorAction Stop | Select-Object Name, VideoProcessor -ErrorAction Stop
             #Monitor
-            $monitor = Get-CimInstance -Class WmiMonitorID -Namespace root\wmi -ComputerName $hostName | Select-Object @{Label='ManufacturerName'; Expression={($_.ManufacturerName | Foreach-Object {[char]$_}) -join ""}}, 
+            $monitor = Get-CimInstance -Class WmiMonitorID -Namespace root\wmi -CimSession $cimsession -ErrorAction SilentlyContinue | Select-Object @{Label='ManufacturerName'; Expression={($_.ManufacturerName | Foreach-Object {[char]$_}) -join ""}}, 
                     @{Label='ProductCodeID'; Expression={($_.ProductCodeID | Foreach-Object {[char]$_}) -join ""}}, 
                     @{Label='UserFriendlyName'; Expression={($_.UserFriendlyName | Foreach-Object {[char]$_}) -join ""}}, 
                     @{Label='SerialNumberID'; Expression={($_.SerialNumberID | Foreach-Object {[char]$_}) -join ""}}, 
                     YearOfManufacture, WeekOfManufacture -ErrorAction Stop
             #https://www.lansweeper.com/knowledgebase/list-of-3-letter-monitor-manufacturer-codes/
             #Disk
-            $diskDrive = Get-CimInstance -ClassName Win32_DiskDrive -ComputerName $hostName | Select-Object Model, SerialNumber, Size, FirmwareRevision, InterfaceType, Index -ErrorAction Stop
+            $diskDrive = Get-CimInstance -ClassName Win32_DiskDrive -CimSession $cimsession -ErrorAction Stop | Select-Object Model, SerialNumber, Size, FirmwareRevision, InterfaceType, Index -ErrorAction Stop
             #Network Adapter
-            $networkAdapter = Get-CimInstance -ClassName Win32_NetworkAdapter -ComputerName $hostName | Where-Object {$_.PhysicalAdapter -eq $true} | Select-Object Name, ProductName, DeviceID, Speed, AdapterType, InterfaceIndex, MACAddress -ErrorAction Stop
+            $networkAdapter = Get-CimInstance -ClassName Win32_NetworkAdapter -CimSession $cimsession -ErrorAction Stop | Where-Object {$_.PhysicalAdapter -eq $true} | Select-Object Name, ProductName, DeviceID, Speed, AdapterType, InterfaceIndex, MACAddress -ErrorAction Stop
             $objInv = New-Object psobject
             $objInv | Add-Member -Name ComputerName -MemberType NoteProperty -Value $hostName
             $objInv | Add-Member -Name computerSystem -MemberType NoteProperty -Value $computerSystem
@@ -53,6 +52,7 @@ foreach ($computerName in $servers) {
             $objInv | Add-Member -Name diskDrive -MemberType NoteProperty -Value $diskDrive
             $objInv | Add-Member -Name networkAdapter -MemberType NoteProperty -Value $networkAdapter
             $result += $objInv
+
         } #try
         catch
         {
@@ -65,10 +65,21 @@ foreach ($computerName in $servers) {
     }
 } #foreach ($computerName in $servers) {
 $rawJson = (($result | ConvertTo-Json -Depth 3).replace('\u0000', '')) -split "`r`n"
+if ($rawJson[0] -eq '[')
+{
+    $formatedJson = .{
+        'var data = ['
+        $rawJson | Select-Object -Skip 1
+    } #replace first Line
+    $formatedJson[-1] = '];' #replace last Line
+}
+else 
+{
+    $formatedJson = .{
+        'var data = {'
+        $rawJson | Select-Object -Skip 1
+    } #replace first Line
+    $formatedJson[-1] = '};' #replace last Line
+}
 
-$formatedJson = .{
-    'var data = ['
-    $rawJson | Select-Object -Skip 1
-} #replace first Line
-$formatedJson[-1] = '];' #replace last Line
-$formatedJson | Out-File $PSScriptRoot\data.js
+$formatedJson | Out-File $PSScriptRoot\data.js -Force
